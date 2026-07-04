6111a6a896adb5d5cd877493bf8e7f63a4e17c61
manhtranduc <manhtranduc0202@gmail.com>
2026-07-04 15:13:29 +0700

    fix prettier

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get } from '@nestjs/common'
-import { AppService } from './app.service'
 import { ApiOperation } from '@nestjs/swagger'
+
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) {}
 
-  @ApiOperation({
-    summary: 'Get Hello message',
-    description: 'This endpoint returns a hello message from the service.'
-  })
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
-  }
+	@ApiOperation({
+		summary: 'Get Hello message',
+		description: 'This endpoint returns a hello message from the service.'
+	})
+	@Get()
+	getHello(): string {
+		return this.appService.getHello()
+	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { ConfigModule } from '@nestjs/config'
-import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true
-    }),
-    AuthModule
-  ],
-  controllers: [AppController],
-  providers: [AppService]
+	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true
+		}),
+		AuthModule
+	],
+	controllers: [AppController],
+	providers: [AppService]
 })
 export class AppModule {}
